038542e87e55ac3c7d162d7256c1046f21d9a827
trevisanj <juliotrevisan@gmail.com>
2017-10-09 20:16:43 -0300

    ...

--- a99/gui/a_WSelectFileOrDir.py
+++ a99/gui/a_WSelectFileOrDir.py
@@ -48,29 +48,23 @@ class _WSelectFileOrDir(WBase):
         # lw.addWidget(t)
 
         e = self.edit = QLineEdit()
-        e.textEdited.connect(self._edit_changed)
+        e.textEdited.connect(self._on_text_edited)
         lw.addWidget(e)
         # e.setReadOnly(True)
 
         b = self.button = QToolButton()
-        b.clicked.connect(self.on_button_clicked)
+        b.clicked.connect(self._on_button_clicked)
         lw.addWidget(b)
         b.setFixedWidth(30)
 
         # Forces paint red if invalid
-        self._edit_changed()
+        self._on_text_edited()
 
-    def on_button_clicked(self, _):
-        self._on_button_clicked()
+    def _on_button_clicked(self, _):
+        self._act_on_button_clicked()
 
-    def _edit_changed(self):
-        exists = os.path.isfile(self._get_value())
-        if not exists and self._last_exists:
-            self._last_exists = False
-            self.changed.emit()
-        elif exists:
-            self._last_exists = True
-            self._wanna_emit()
+    def _on_text_edited(self):
+        self._act_on_change()
 
     def _update_gui(self):
         a99.style_widget_valid(self.edit, self._flag_valid)
@@ -92,6 +86,23 @@ class _WSelectFileOrDir(WBase):
         self._last_value = value
         self._last_exists = os.path.isfile(value)
 
+    def _act_on_button_clicked(self):
+        raise NotImplementedError()
+
+    def _set_text_from_file_dialog(self, text):
+        self.edit.setText(text)
+        self.dialog_path = text
+        self._act_on_change()
+
+    def _act_on_change(self):
+        exists = os.path.isfile(self._get_value())
+        if not exists and self._last_exists:
+            self._last_exists = False
+            self.changed.emit()
+        elif exists:
+            self._last_exists = True
+            self._wanna_emit()
+
 
 class WSelectFile(_WSelectFileOrDir):
     def __init__(self, *args, **kwargs):
@@ -106,8 +117,7 @@ class WSelectFile(_WSelectFileOrDir):
         res = QFileDialog.getOpenFileName(self, self.dialog_title, path_,
                                           self.dialog_wild)[0]
         if res:
-            self.edit.setText(res)
-            self.dialog_path = res
+            self._set_text_from_file_dialog(res)
 
 
 class WSelectDir(_WSelectFileOrDir):
@@ -116,14 +126,14 @@ class WSelectDir(_WSelectFileOrDir):
         self.button.setIcon(a99.get_icon("folder-change"))
         self.edit.setText(self.dialog_path)
 
-    def _on_button_clicked(self):
+    def _act_on_button_clicked(self):
         path_ = self.edit.text().strip()
         if len(path_) == 0:
             path_ = self.dialog_path
         res = QFileDialog.getExistingDirectory(self, self.dialog_title, path_)
         if len(res) > 0:
-            self.edit.setText(res)
-            self.dialog_path = res
+            self._set_text_from_file_dialog(res)
+
 
     def _validate(self):
         return os.path.isdir(self.value)
